1bfc7aaa6fb050f55b799be1967c2f3630a0a80f
Roman Diakonov <i@rodrix.ru>
2025-09-26 21:44:34 +0900

    refactor: Moved generic_use_case.dart

--- lib/features/auth/domain/user_cases/put_local_phone_number_use_case.dart
+++ lib/features/auth/domain/user_cases/put_local_phone_number_use_case.dart
@@ -1,5 +1,5 @@
 import 'package:injectable/injectable.dart';
-import 'package:vishka_front_v3/shared/use_cases/generic_use_case.dart';
+import 'package:vishka_front_v3/core/use_cases/generic_use_case.dart';
 import 'package:vishka_front_v3/features/auth/domain/repositories/user_repository.dart';
 
 @injectable
--- lib/features/auth/domain/user_cases/send_remote_phone_number_use_case.dart
+++ lib/features/auth/domain/user_cases/send_remote_phone_number_use_case.dart
@@ -1,6 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:vishka_front_v3/core/states/network_data_state.dart';
-import 'package:vishka_front_v3/shared/use_cases/generic_use_case.dart';
+import 'package:vishka_front_v3/core/use_cases/generic_use_case.dart';
 import 'package:vishka_front_v3/features/auth/domain/repositories/auth_repository.dart';
 
 @injectable
